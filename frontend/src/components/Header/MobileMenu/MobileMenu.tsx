1d0f6d07161850e553bf99cc9c53753590736928
BotalovSN <ctulhufromrlieh@gmail.com>
2024-04-14 01:58:09 +0500

    - Mobile menu design made.

--- frontend/src/components/Header/MobileMenu/MobileMenu.tsx
+++ frontend/src/components/Header/MobileMenu/MobileMenu.tsx
@@ -5,6 +5,7 @@ import classes from "./MobileMenu.module.scss";
 import menuLogoMobile from "../../../img/logo-menu-mobile.module.png";
 import { privateRoutes, publicRoutes } from "../../../router";
 import Navbar, { BeforeNavigateHandler } from "../../UI/Navbar/Navbar";
+import { Link } from "react-router-dom";
 
 interface MobileMenuData {
     setVisible: (value: boolean) => void;
@@ -30,7 +31,10 @@ const MobileMenu: React.FC<MobileMenuData> = ({setVisible}) => {
                 <button className={classes.menu_close_btn} onClick={() => setVisible(false)}/>
             </div>
             <Navbar routes={usedRoutes} listClassName={classes.menu_list} itemClassName={classes.menu_item} beforeNavigateHandler={beforeNavigateHandler} />
-            <div>Основная часть</div>
+            <div className={classes.sign_div}>
+                <Link className={classes.signup_link} to="/login">Зарегистрироваться</Link>
+                <button className={classes.signin_button}>Войти</button>
+            </div>
         </div>
     );
 }
